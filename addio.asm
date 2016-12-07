241e45c4f7624955c0a87697aace122348424d28
Tianyu0312 <tl1@andrew.cmu.edu>
2016-12-06 22:55:33 -0500

    Update addio.asm

--- addio.asm
+++ addio.asm
@@ -2,9 +2,11 @@
         ; {R0, R1} = {R0, R1} + {R6, R7}
 
         .ORG $0
+        
 start1  LDI R2, $0003   ;
         LDA R0, $2000   ;
         AND R2, R0      ;
+        CMI R2, $0      ;
         BRZ start2      ;
         BRA start1      ;
         
@@ -15,8 +17,7 @@ start2  LDI R2, $0003   ;
         BRZ start3      ;
         BRA start2      ;
         
-start3  STA $2000, R4   ;
-        LDI R2, $0003   ;
+start3  LDI R2, $0003   ;
         LDA R6, $2000   ;
         AND R2, R6      ;
         CMI R2, $2      ;
@@ -33,4 +34,36 @@ start4  LDI R2, $0003   ;
 done    .DW $3106       ;  
         STA $2000, R0   ;
         MOV R4, R1      ;
-        BRA first       ;
+        
+start5  LDI R2, $0003   ;
+        LDA R0, $2000   ;
+        AND R2, R0      ;
+        CMI R2, $0      ;
+        BRZ start6      ;
+        BRA start5      ;
+        
+start6  LDI R2, $0003   ;
+        LDA R1, $2000   ;
+        AND R2, R1      ;
+        CMI R2, $1      ;
+        BRZ start7      ;
+        BRA start6      ;
+        
+start7  LDI R2, $0003   ;
+        LDA R6, $2000   ;
+        AND R2, R6      ;
+        CMI R2, $2      ;
+        BRZ start8      ;
+        BRA start7      ;
+        
+start8  LDI R2, $0003   ;
+        LDA R7, $2000   ;
+        AND R2, R7      ;
+        CMI R2, $3      ;
+        BRZ done        ;
+        BRA start8      ;
+        
+done	  .DW $3106
+	  STA $2000, R0
+	  STA $2000, R1
+	  STOP
